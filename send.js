96fffee52b85b9ad8d6fa1b0e311b8d6db4d3582
Eisbehr <git@eisbehr.de>
2016-10-20 16:24:35 +0200

    0.1.2 - return requested message object in response - check if message parameter is set on request - added full documentation

--- send.js
+++ send.js
@@ -2,11 +2,17 @@
 
 var Pushover = require("pushover-notifications");
 var message = require("./src/message");
+var merge = require("./src/merge");
 var json = require("./src/json");
 
 module.exports = function(config, params, respond, error) {
     // noinspection JSUnresolvedVariable
     var appName = params.app || params.a || "default";
+
+    // noinspection JSUnresolvedVariable
+    if( !params.message && !params.m ) {
+        return error("parameter 'message' is required");
+    }
 
     // noinspection JSUnresolvedVariable
     if( !config.apps[appName] ) {
@@ -23,13 +29,14 @@ module.exports = function(config, params, respond, error) {
     var messageObj = message(config, params, error);
 
     // send message
-    pushover.send(messageObj, function(err, result) {
+    pushover.send(merge({}, messageObj), function(err, result) {
         if( err ) {
             return error(err.message);
         }
 
         respond({
             success: true,
+            request: messageObj,
             pushover: json(result) || result
         });
     });
